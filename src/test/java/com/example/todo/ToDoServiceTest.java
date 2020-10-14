a5776d13b7d85cc838e47d70cddccde15de71798
Dzaki Buchori <dzakibuchori@gmail.com>
2020-10-14 11:17:55 +0700

    add ToDoServiceTest

---
+++ src/test/java/com/example/todo/ToDoServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ToDoServiceTest {
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @Test
+    void getAllToDos() {
+        ToDo toDoSample = new ToDo("Todo Sample 1", true);
+        toDoRepository.save(toDoSample);
+        ToDoService toDoService = new ToDoService(toDoRepository);
+
+        List<ToDo> toDoList = toDoService.findAll();
+        ToDo lastToDo = toDoList.get(toDoList.size() - 1);
+
+        assertEquals(toDoSample.getText(), lastToDo.getText());
+        assertEquals(toDoSample.isCompleted(), lastToDo.isCompleted());
+        assertEquals(toDoSample.getId(), lastToDo.getId());
+    }
+}
